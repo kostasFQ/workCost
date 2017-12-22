c84cee03bb67cf4e694fb8ec7e178338259a0436
kostasFQ <kostasfq@gmail.com>
2017-12-23 01:59:03 +0300

    zz

--- client/clientScript.js
+++ client/clientScript.js
@@ -1,38 +1,76 @@
-
-
-function push(){
+function post(){
     let costInput = document.getElementById('InputCurrencyValue').value;
     let currency = document.getElementById('currency').value;
     if(!/\D/.test(costInput) && costInput.length > 2) { 
         let price =  costInput+' '+currency;
         
-        
-        
         let xhr = new XMLHttpRequest();
         xhr.open('POST','/cur', true);
         xhr.setRequestHeader('Content-Type', 'text/plain');
-        /*xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4 && xhr.status === 200) {
-                console.log(xhr.responseText);
-            }
-        }*/
         xhr.send(price);
-
-
     } else { alert('wrong!') }
 
 };
 
-function get() {
-    let xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById('ansverCost').innerHTML = xhr.responseText;
-            console.log(xhr.responseText);
+function go() {
+    function get() {
+        let xhr = new XMLHttpRequest();
+    
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                let ansver = xhr.responseText;
+                console.log(JSON.parse(ansver));
+                inSecond = JSON.parse(ansver).sec;
+            }
         }
+        xhr.open('GET', '/cur', true);
+        xhr.send();
     }
-    xhr.open('GET', '/cur', true);
+
+    function summ() {
+        let secs = 0;
+        setInterval(function(){
+            secs++;
+            cost += +inSecond;
+            timer.innerHTML = (cost/10).toFixed(4) + ' за - ' + (secs/10).toFixed(0) + 'секунд';
+        }, 100);
+    };
+
+    let timer = document.getElementById('timer');
+    let inSecond;
+    let cost = 0;
+
+    get();
+    summ();
+};
+
+
+window.onload = function() {
+    getCurrency();
+};
+function getCurrency(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0', true);
     xhr.send();
-}
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                let currency = JSON.parse( xhr.responseText);
+                //console.log(currency);
+                currency.map( function(val, i) {
+                    if(val.Cur_Abbreviation === "USD" || 
+                    val.Cur_Abbreviation === "EUR" || 
+                    val.Cur_Abbreviation === "RUB" ||
+                    val.Cur_Abbreviation === "UAH"
+                    ) {
+                        console.log(val.Cur_Name, val.Cur_OfficialRate, val.Cur_Scale);
+                    };
+                    
+                } );
+
+            }
+        }
+        
+        
+};
 
--- server/server.js
+++ server/server.js
@@ -1,33 +1,30 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+let cors = require('cors');
 
-const zet = 'zet!';
-let arr = [];
+let price = [];
 
 app.use(express.static('client'));
-
-app.use(function(req, res, next){
-    if (req.is('text/*')) {
-      req.text = '';
-      req.setEncoding('utf8');
-      req.on('data', function(chunk){ req.text += chunk });
-      req.on('end', next);
-    } else {
-      next();
-    }
-  });
-
-app.get('/cur', function(req, res) {
-    res.send(arr);
-    res.end();
-} );
+app.use(bodyParser.text());
 
 app.post('/cur', function(req, res) {
-    arr.push(req.text);
-    console.log(arr);
+    price = req.body.split(' ');
+    console.log(price);
 } );
 
+app.get('/cur', function(req, res) {
+    let day = (+price[0]/21).toFixed(2);
+    let hour = (day/8).toFixed(2);
+    let min = (hour/60).toFixed(2);
+    let sec = (min/60).toFixed(5);
+    res.send( {
+        day : day,
+        hour : hour,
+        min : min,
+        sec : sec,
+    }  );
+} );
 
 const server = app.listen(process.env.PORT || 8000, function() {
     console.log('Server running on port 8000.')
