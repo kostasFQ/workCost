603038a3941e29423625a56170cd69dc08476e53
kostasFQ <kostasfq@gmail.com>
2017-12-22 13:41:01 +0300

    z

--- client/clientScript.js
+++ client/clientScript.js
@@ -1,11 +1,38 @@
 
 
 function push(){
-    let message;
     let costInput = document.getElementById('InputCurrencyValue').value;
     let currency = document.getElementById('currency').value;
     if(!/\D/.test(costInput) && costInput.length > 2) { 
-        document.getElementById('ansverCost').innerHTML = costInput+' '+currency;
+        let price =  costInput+' '+currency;
+        
+        
+        
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST','/cur', true);
+        xhr.setRequestHeader('Content-Type', 'text/plain');
+        /*xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        }*/
+        xhr.send(price);
+
+
     } else { alert('wrong!') }
+
 };
+
+function get() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('ansverCost').innerHTML = xhr.responseText;
+            console.log(xhr.responseText);
+        }
+    }
+    xhr.open('GET', '/cur', true);
+    xhr.send();
+}
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+
+const zet = 'zet!';
+let arr = [];
 
 app.use(express.static('client'));
 
-app.get('/cur');
+app.use(function(req, res, next){
+    if (req.is('text/*')) {
+      req.text = '';
+      req.setEncoding('utf8');
+      req.on('data', function(chunk){ req.text += chunk });
+      req.on('end', next);
+    } else {
+      next();
+    }
+  });
+
+app.get('/cur', function(req, res) {
+    res.send(arr);
+    res.end();
+} );
+
+app.post('/cur', function(req, res) {
+    arr.push(req.text);
+    console.log(arr);
+} );
 
 
 const server = app.listen(process.env.PORT || 8000, function() {
